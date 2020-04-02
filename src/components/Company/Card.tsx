406d6c516c14678130c56f20b3da292f18b2d69d
Spade, Sean <sspade@axcess-financial.com>
2020-04-02 11:58:32 -0400

    feat: Adds ability to delete companies

--- src/components/Company/Card.tsx
+++ src/components/Company/Card.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import * as mutations from "../../graphql/mutations";
+import { useMutation } from "../../graphql/hooks/useMutation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   company: {
@@ -18,19 +22,25 @@ interface Props {
 }
 
 const Card = ({ company }: Props) => {
-  const { name, description, logoUrl } = company;
+  const { name, description, logoUrl, id } = company;
+  const [{ loading }, deleteCompany] = useMutation(mutations.deleteCompany);
   return (
     <div>
+      {loading && <div>Deleting...</div>}
       <div className="flex flex-col justify-center items-center rounded w-56 p-2 hover:shadow-md zoom">
-        <img
-          className="w-full h-20 border-2 rounded"
-          src={logoUrl}
-          alt=""
-        />
+        <img className="w-full h-20 border-2 rounded" src={logoUrl} alt="" />
         <div className=" mt-1 text-gray-900 flex justify-center font-bold w-20">
           {name}
         </div>
         <div className="flex items-center ml-4">{description}</div>
+      </div>
+      <div>
+        <FontAwesomeIcon
+          icon={faTimes}
+          onClick={() => {
+            deleteCompany({ input: { id } });
+          }}
+        />
       </div>
     </div>
   );
--- src/components/Company/Row.tsx
+++ src/components/Company/Row.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTimes} from "@fortawesome/free-solid-svg-icons";
+import {useMutation} from "../../graphql/hooks/useMutation";
+import * as mutations from "../../graphql/mutations";
 
 interface Props {
   company: {
@@ -16,7 +20,8 @@ interface Props {
   };
 }
 const CompanyRow = ({ company }: Props) => {
-  const { name, description, logoUrl } = company;
+  const { name, description, logoUrl, id } = company;
+  const [{ loading }, deleteCompany] = useMutation(mutations.deleteCompany);
   return (
     <div className="rounded w-full flex mb-1 hover:shadow-md zoom">
       <div className="flex p-2">
@@ -29,6 +34,14 @@ const CompanyRow = ({ company }: Props) => {
           {name}
         </div>
         <div className="flex items-center ml-4">{description}</div>
+      </div>
+      <div>
+        <FontAwesomeIcon
+          icon={faTimes}
+          onClick={() => {
+            deleteCompany({ input: { id } });
+          }}
+        />
       </div>
     </div>
   );
